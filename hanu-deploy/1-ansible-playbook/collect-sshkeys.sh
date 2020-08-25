bd18acb2fb5128b3ad4fd605f527a4afd4829e2b
root <dubaek@gmail.com>
2020-08-25 13:44:07 +0900

    Add collect-sshkeys.sh script

---
+++ hanu-deploy/1-ansible-playbook/collect-sshkeys.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ldapsearch -o ldif-wrap=no -xLLL -h 172.16.1.2 -b "dc=hanu-ci,dc=io" -D "cn=admin,dc=hanu-ci,dc=io" -W "objectclass=ldapPublicKey" | grep sshPublicKey | sed "s/sshPublicKey: //"
