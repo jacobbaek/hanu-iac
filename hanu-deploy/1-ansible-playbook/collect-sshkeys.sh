bae6b4d1e341c368339029996674e3a604d78361
root <dubaek@gmail.com>
2020-08-25 04:49:32 +0000

    Update collect-sshkeys.sh

--- hanu-deploy/1-ansible-playbook/collect-sshkeys.sh
+++ hanu-deploy/1-ansible-playbook/collect-sshkeys.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ldapsearch -o ldif-wrap=no -xLLL -h 172.16.1.2 -b "dc=hanu-ci,dc=io" -D "cn=admin,dc=hanu-ci,dc=io" -W "objectclass=ldapPublicKey" | grep sshPublicKey | sed "s/sshPublicKey: //"
+RST=`ldapsearch -o ldif-wrap=no -xLLL -h 172.16.1.2 -b "dc=hanu-ci,dc=io" -D "cn=admin,dc=hanu-ci,dc=io" -W "objectclass=ldapPublicKey" | grep sshPublicKey | sed "s/sshPublicKey: //" | grep ssh-rsa`
+echo $RST > authorized_keys
